c16193a2a0aa026c81f8cd5274ef37330b2e80fd
kamsiOrazulike <kamsi_orazulike@icloud.com>
2023-05-09 14:10:21 +0100

    loader added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,32 @@
-import '@/styles/hero.css'
+import "@/styles/hero.css";
 import "@/styles/globals.css";
-import Layout from "@/components/layout";
+import "@/styles/loader.css";
+import "@/styles/card.css";
+import { useState, useEffect } from "react";
+import Layout from "@/components/Layout";
+import LoadingPage from "@/components/Loading";
 
 //Responsible for rendering all of my pages by passing component and page props
 export default function App({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Change this value to set the duration of the loading page
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {loading ? (
+        <LoadingPage />
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
+    </>
   );
 }
