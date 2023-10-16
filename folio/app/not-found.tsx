eb75b9624f342bacf75b4d862a8a1f08c8e1a04a
kamsiOrazulike <kamsi_orazulike@icloud.com>
2023-10-16 14:03:50 +0100

    404 page edits

--- folio/app/not-found.tsx
+++ folio/app/not-found.tsx
@@ -3,15 +3,15 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <>
-      <div className="bg-404bg bg-cover bg-no-repeat h-screen">
-        <div className="min-h-screen flex flex-col items-center justify-center bg-black/60">
+      <div className="bg-404bg bg-cover bg-center bg-no-repeat h-screen">
+        <div className="min-h-screen flex flex-col items-center text-center justify-center bg-black/60">
           <h2 className="text-4xl font-bold mb-4 text-gray-200">
             Oops! You now have to choose...
           </h2>
           <p className="text-gray-300 mb-8">
             Sorry, the requested page is still in the works! Bear with me :)
           </p>
-          <div className="w-1/3 flex flex-row justify-between">
+          <div className="w-[80%] md:w-1/3 flex flex-row justify-between text-xs md:text-md">
             <Link
               href="/"
               className="rounded-full px-4 py-2 text-white bg-red-400 hover:bg-red-600 hover:scale-105 transition-all duration-150"
@@ -19,7 +19,7 @@ export default function NotFound() {
               Return Home
             </Link>
             <Link
-              href="/"
+              href="/projects"
               className="rounded-full px-4 py-2 text-white bg-blue-400 hover:bg-blue-600 hover:scale-105 transition-all duration-150"
             >
               Head to projects
